424d4f726d1eaa908ff9b271232edbba7a9903ad
Prajith <kprajith@amazon.com>
2021-03-07 22:30:23 +0000

    Adding Get and Set

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -1,11 +1,64 @@
-//! Get command 
+//! Get command. See [Get command](https://redis.io/commands/get) for official documentation
 
-/// Holds the data for [Set command](super::Command::Set)
+use std::collections::LinkedList;
+
+use crate::resp::{Type, TypeConsumer};
+use crate::Result;
+/// Holds key required for the [Get command](super::Command::Get)
 #[derive(Debug, PartialEq)]
 pub struct Get {
-    key: String
+    key: String,
 }
 
 impl Get {
-    
+    /// Returns an instance of [super::get::Get] 
+    pub fn from(type_consumer: &mut TypeConsumer) -> Result<Self> {
+        let key = type_consumer.next_string()?;
+        Ok(Get { key })
+    }
+}
+
+impl From<Get> for Type {
+    fn from(get: Get) -> Self {
+        let mut ll = LinkedList::new();
+        ll.push_back(Type::BulkString(b"GET".to_vec()));
+        ll.push_back(Type::BulkString(get.key.into_bytes()));
+        Type::Array(ll)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::resp::{Type, TypeConsumer};
+
+    use super::Get;
+
+    #[test]
+    fn from_works() {
+        let mut tc = TypeConsumer::new(Type::BulkString(b"Hello".to_vec()));
+        let get = Get::from(&mut tc).unwrap();
+        assert_eq!(
+            get,
+            Get {
+                key: "Hello".into()
+            }
+        )
+    }
+
+    #[test]
+    fn into_works() {
+        let get = Get {
+            key: "Hello".into(),
+        };
+        let t: Type = get.into();
+        let expected = Type::Array(
+            vec![
+                Type::BulkString(b"GET".to_vec()),
+                Type::BulkString(b"Hello".to_vec()),
+            ]
+            .into_iter()
+            .collect(),
+        );
+        assert_eq!(t, expected);
+    }
 }
--- src/commands/set.rs
+++ src/commands/set.rs
@@ -1,7 +1,75 @@
-//! Set command 
-/// Holds the data for [Get command](super::Command::Get)
-#[derive(Debug)]
+//! Set command. See [Set command](https://redis.io/commands/set) for official documentation
+
+use std::collections::LinkedList;
+
+use crate::resp::{Type, TypeConsumer};
+use crate::Result;
+/// Holds key and value required for the [Set command](super::Command::Set)
+#[derive(Debug, PartialEq)]
 pub struct Set {
     key: String,
-    value: String
+    value: String,
+}
+
+impl Set {
+    /// Returns an instance of [super::get::Get] 
+    pub fn from(type_consumer: &mut TypeConsumer) -> Result<Self> {
+        let key = type_consumer.next_string()?;
+        let value = type_consumer.next_string()?;
+        Ok(Set { key, value })
+    }
+}
+
+impl From<Set> for Type {
+    fn from(get: Set) -> Self {
+        let mut ll = LinkedList::new();
+        ll.push_back(Type::BulkString(b"SET".to_vec()));
+        ll.push_back(Type::BulkString(get.key.into_bytes()));
+        ll.push_back(Type::BulkString(get.value.into_bytes()));
+        Type::Array(ll)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::resp::{Type, TypeConsumer};
+
+    use super::Set;
+
+    #[test]
+    fn from_works() {
+        let mut tc = TypeConsumer::new(Type::Array(vec![
+            Type::BulkString(b"Hello".to_vec()),
+            Type::BulkString(b"World".to_vec()),
+        ]
+        .into_iter()
+        .collect()));
+        let set = Set::from(&mut tc).unwrap();
+        assert_eq!(
+            set,
+            Set {
+                key: "Hello".into(),
+                value: "World".into()
+            }
+        )
+    }
+
+    #[test]
+    fn into_works() {
+        let set = Set {
+            key: "Hello".into(),
+            value: "World".into(),
+        };
+        let t: Type = set.into();
+        let expected = Type::Array(
+            vec![
+                Type::BulkString(b"SET".to_vec()),
+                Type::BulkString(b"Hello".to_vec()),
+                Type::BulkString(b"World".to_vec()),
+            ]
+            .into_iter()
+            .collect(),
+        );
+        assert_eq!(t, expected);
+    }
 }
